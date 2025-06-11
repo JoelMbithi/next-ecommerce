bd36e866ac084ff569d6b525dc8b64109bab9520
Joel Mbithi <joellembithi@gmail.com>
2025-06-11 19:42:22 +0300

    Done

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+/* load enviroment variable */
+dotenv.config()
+
+const app = express()
+const PORT = 4000
+
+///MiddleWare setUp
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods:["GET",'POST',"PUT","DELETE","PATCH"],
+    allowedHeaders:["Content-type", "Authorization"],
+    Credentials:true
+}))
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`Server is running on well on ${PORT}`)
+})
